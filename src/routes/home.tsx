d7a4d427f04a08dc3dec71c5a946530aabc06c31
seolwu <ihyeon9021@gmail.com>
2025-04-01 16:22:18 +0900

    change: hover on <a> in section 2, underline

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -51,7 +51,7 @@ export default class HomeRoute extends Component {
                 </span>
                 <br/>
                 <span class='text-on-background/95 text-center'>
-                  In <a href='/blog' class='text-rose-400'>Blog</a>,
+                  In <a href='/blog' class='text-rose-400 hover:underline underline-offset-2'>Blog</a>,
                   we will share various <b class='text-on-background'>news</b>, <b class='text-on-background'>updates</b>, <b class='text-on-background'>events</b>, and <b class='text-on-background'>developer strategy guide</b>.
                 </span>
                 <span class='text-on-background/95 text-center'>
