370584f0ef9148b0d65bd9b3cad7a2dd4450b584
seolwu <ihyeon9021@gmail.com>
2025-04-02 20:59:10 +0900

    change: remove: for debug

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -6,8 +6,6 @@ interface StorageReference {
 }
 
 const storage = (type: Storage, name: string): StorageReference => {
-  console.log(type)
-
   return {
     set(key, value) {
       type.setItem(key, value)
